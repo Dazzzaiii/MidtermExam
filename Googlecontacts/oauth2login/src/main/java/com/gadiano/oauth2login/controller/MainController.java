2547164212bef92917d65c903686f4bf4d79a04e
Dazzzaiii <152935237+Dazzzaiii@users.noreply.github.com>
2025-03-06 23:41:36 +0800

    Integrated google contacts and UI

--- Googlecontacts/oauth2login/src/main/java/com/gadiano/oauth2login/controller/MainController.java
+++ Googlecontacts/oauth2login/src/main/java/com/gadiano/oauth2login/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.arela.oauth2login.controller;
+package com.gadiano.oauth2login.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- Googlecontacts/oauth2login/src/main/java/com/gadiano/oauth2login/controller/UserController.java
+++ Googlecontacts/oauth2login/src/main/java/com/gadiano/oauth2login/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.arela.oauth2login.controller;
+package com.gadiano.oauth2login.controller;
 
 import java.util.Map;
 
